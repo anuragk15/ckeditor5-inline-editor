1d4c753cd6d975470fd7fdecd11990668073e57c
Anurag Kanoria <anuragkanoria@Anurags-MacBook-Air.local>
2021-12-08 01:51:11 +0530

    fixed sanitize html, removed it.

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -162,20 +162,6 @@ Editor.defaultConfig = {
 		]
 	},
 	
-	htmlEmbed: {
-		showPreviews: true,
-		sanitizeHtml: (inputHtml) => {
-			// Strip unsafe elements and attributes, e.g.:
-			// the `<script>` elements and `on*` attributes.
-			const outputHtml = sanitize(inputHtml);
-
-			return {
-				html: outputHtml,
-				// true or false depending on whether the sanitizer stripped anything.
-				hasChanged: true
-			};
-		}
-	},
 	mediaEmbed: {
 		previewsInData: true,
 	},
